78609f930c76abdb5cf41b3cc4db8918c02d6226
ANDY199106 <98603840+ANDY199106@users.noreply.github.com>
2023-02-02 11:41:59 +0200

    js button

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@ document.querySelector('#whatzapButton')
     .addEventListener('click', () => {
         window.location.href = "https://wa.me/+40748527089";
     });
+
+document.querySelector('#locationButton')
+    .addEventListener('click', () => {
+        window.location.href = "https://goo.gl/maps/c8aM4j3GDJuexaTh6";
+    });
+document.querySelector('#contButton')
+    .addEventListener("click",  () => {
+        scroll(0, 20000, 'smooth');
+    });
+document.querySelector('#rezerva_acuma')
+    .addEventListener('click', () => {
+        window.location.href = "https://wa.me/+40748527089" ;
+    });
+document.querySelector('#btn_obiective')
+    .addEventListener('click', () => {
+        window.location.href = "obiectiveTuristice.html" ;
+    });
+document.querySelector('#main_page')
+    .addEventListener('click', () => {
+        window.location.href = "index.html";
+    });
