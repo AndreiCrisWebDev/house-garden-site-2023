fb8b2a4a7f3ef681c61801b1e0379f2a9837cc39
ANDY199106 <98603840+ANDY199106@users.noreply.github.com>
2023-02-07 22:11:40 +0200

    cred ca am rezolvat  problema cu butttonul

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
-document.querySelector('#whatzapButton')
-    .addEventListener('click', () => {
-        window.location.href = "https://wa.me/+40748527089";
-    });
+document.querySelector("#whatzapButton").addEventListener("click", () => {
+  window.location.assign("https://wa.me/+40748527089");
+});
 
-document.querySelector('#locationButton')
-    .addEventListener('click', () => {
-        window.location.href = "https://goo.gl/maps/c8aM4j3GDJuexaTh6";
-    });
-document.querySelector('#contButton')
-    .addEventListener("click",  () => {
-        scroll(0, 20000, 'smooth');
-    });
-document.querySelector('#rezerva_acuma')
-    .addEventListener('click', () => {
-        window.location.href = "https://wa.me/+40748527089" ;
-    });
-document.querySelector('#btn_obiective')
-    .addEventListener('click', () => {
-        window.location.href = "obiectiveTuristice.html" ;
-    });
-document.querySelector('#main_page')
-    .addEventListener('click', () => {
-        window.location.href = "index.html";
-    });
+document.querySelector("#locationButton").addEventListener("click", () => {
+  window.location.href = "https://goo.gl/maps/c8aM4j3GDJuexaTh6";
+});
+document.querySelector("#contButton").addEventListener("click", () => {
+  scroll(0, 20000, "smooth");
+});
+document.querySelector("#rezerva_acuma").addEventListener("click", () => {
+  window.location.href = "https://wa.me/+40748527089";
+});
+document.querySelector("#btn_obiective").addEventListener("click", () => {
+  window.location.href = "obiectiveTuristice.html";
+});
+// const homeBtn = document.getElementById("main_page");
+// addEventListener("click", () => {
+//   window.location.href = "index.html";
+//});
+function homePage() {
+  window.location.assign("index.html");
+}
