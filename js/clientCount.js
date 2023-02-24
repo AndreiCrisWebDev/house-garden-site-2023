518b32dbfcf800fab54846ea17a18d16556f1969
ANDY199106 <98603840+ANDY199106@users.noreply.github.com>
2023-02-24 09:54:11 +0200

    updating clienti reviu

---
+++ js/clientCount.js
@@ -0,0 +1,13 @@
+//pentru sectia  reviewCount am facut un fisier separat
+
+//// plan  propus
+/// pas 1 selectam h3 care au id clienti, camere, ani
+const clientCount = document.querySelector("#reviewCount h3");
+const nrClienti = parseInt(clientCount.innerText);
+/// pas 2 facem o functie care sa numere de la 0 la valoarea id
+/// pas 3 facem un timer care sa faca numaratoarea fluida
+/// pas 4 metoda ca animatia sa inceapa doar cand ajungem la acel id pe pagina
+
+count = (i) => {
+  for (let i = 0; i < nrClienti.length; i++) {}
+};
