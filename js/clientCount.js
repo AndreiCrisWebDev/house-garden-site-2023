d24bdb120a32cce6b6d0186df087861e87cf909a
ANDY199106 <98603840+ANDY199106@users.noreply.github.com>
2023-02-26 18:38:22 +0200

    the review counter works

--- js/clientCount.js
+++ js/clientCount.js
@@ -2,32 +2,42 @@
 
 //// plan  propus
 /// pas 1 selectam h3 care au id clienti, camere, ani
-let counter = 109;
-let clientCount = document.querySelector("#reviewCount h3 span");
+
 // const nrClienti = parseInt(clientCount.innerText);
 /// pas 2 facem o functie care sa numere de la 0 la valoarea id
 /// pas 3 facem un timer care sa faca numaratoarea fluida
 /// pas 4 metoda ca animatia sa inceapa doar cand ajungem la acel id pe pagina
 
-// count = (i) => {
-//   for (let i = 0; i < nrClienti.length; i++) {}
-// };
-// (function () {
-//   let sec;
-//   counter = setInterval(() => {
-//     clientCount.innerHTML = sec;
-//     sec++;
-//   }, 100);
-// })();
+// new atempt and it works
+let reviewCount = document.querySelector("#reviewCount");
+let clientCount = document.querySelectorAll(".counter");
 
-etInterval(
-  {
-    function() {
-      counter++;
-      if (counter > "109") {
-        counter = clientCount.innerHTML;
+let speed = 100;
+
+// cand ajunge la id reviewCount
+// este o metoda noua de a vedea unde esti pe view
+
+let CounterObserver = new IntersectionObserver(
+  (entries) => {
+    let [entry] = entries;
+    if (!entry.isIntersecting) return;
+
+    clientCount.forEach((counter) => {
+      function updateCounter() {
+        const targetNumber = +counter.dataset.target;
+        const initialNumber = +counter.innerHTML;
+        const intPerCount = targetNumber / speed;
+        if (initialNumber < targetNumber) {
+          counter.innerHTML = Math.ceil(initialNumber + intPerCount);
+          setTimeout(updateCounter, 150);
+        }
       }
-    },
+      updateCounter();
+    });
   },
-  100
+  {
+    root: null,
+    threshold: 0.4,
+  }
 );
+CounterObserver.observe(reviewCount);
